ac7c3a4891a2c8832f7128b31d9bfe97dbb4186b
blank <blankvip@126.com>
2025-02-27 22:04:42 +0800

    Update MPZLFX.js

--- MPZLFX.js
+++ MPZLFX.js
@@ -70,7 +70,7 @@ function main(config) {
     'DOMAIN-KEYWORD,dicmusic,DIRECT', 
     'DOMAIN-KEYWORD,hdbao,DIRECT', 
     'DOMAIN-KEYWORD,gxnas,DIRECT', 
-    'DOMAIN-KEYWORD,torrenthub,DIRECT'， 
-  );
+    'DOMAIN-KEYWORD,torrenthub,DIRECT'，
+    );
   return config;
 }
