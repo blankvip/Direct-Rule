3367fc6901ea9d69fbb25cd45c974afa57c83cf4
blank <blankvip@126.com>
2026-02-04 23:14:40 +0800

    Update MPZLFX.js

--- MPZLFX.js
+++ MPZLFX.js
@@ -75,7 +75,8 @@ function main(config) {
     'DOMAIN-KEYWORD,hspt,DIRECT',
     'DOMAIN-KEYWORD,cspt,DIRECT',
     'DOMAIN-KEYWORD,ptlover,DIRECT',
-    'DOMAIN-KEYWORD,htpt,DIRECT',
+    'DOMAIN-KEYWORD,htpt,DIRECT'，
+    'DOMAIN-KEYWORD,apphot,DIRECT',
     'DOMAIN-KEYWORD,tmpt,DIRECT'
 );
   return config;
